f3efb3c64aadcea9d1c7f5f1e1260a806e8ba5bf
Shashank Dabral <a0393223@ti.com>
2018-04-12 23:07:15 -0500

    first update

--- src/main.cpp
+++ src/main.cpp
@@ -242,7 +242,7 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
-		double distance_inc = 0.5;
+		double dist_inc = 0.5;
 		for (int i=0;i<50;i++) {
 		    next_x_vals.push_back(car_x + (dist_inc *i)*cos(deg2rad(car_yaw)));
 		    next_y_vals.push_back(car_y + (dist_inc *i)*sin(deg2rad(car_yaw)));
