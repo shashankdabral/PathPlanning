bf305e09f67f8bbf72b4a245983b0c3f8cca1614
Shashank Dabral <a0393223@ti.com>
2018-04-15 14:58:29 -0500

    Added spline smoothening

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -200,6 +201,10 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
+  /* Define reference lanes */
+  int lane =1; /* 0 is leftt, 1 is middle, 2 is right */ 
+  double ref_vel = 49.0;
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -239,6 +244,7 @@ int main() {
 
           	json msgJson;
 
+		int prev_size = previous_path_x.size();
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
@@ -249,6 +255,7 @@ int main() {
 		    next_y_vals.push_back(car_y + (dist_inc *i)*sin(deg2rad(car_yaw)));
 		}
                 #endif
+                #ifdef LN_KEEP
 		for (int i=0;i<50;i++) {
 		    double next_s = car_s + ((i+1) *dist_inc);
 		    /* Calculalte the next-d assuming we are in the middle lane */
@@ -261,7 +268,110 @@ int main() {
 		    next_x_vals.push_back(xy[0]);
 		    next_y_vals.push_back(xy[1]);
 		}
+                #endif
 		
+		/* Define anchor points for spline */
+		vector <double> ptsx;
+		vector <double> ptsy;
+		double ref_x = car_x;
+		double ref_y = car_y;
+		double ref_yaw = deg2rad(car_yaw);
+
+		if (prev_size <2)
+		/* If the list is small just create a previous pt by extrpolating *
+		 * and taking a time step back                                    */
+		{
+		    double prev_car_x = car_x - cos(car_yaw);
+		    double prev_car_y = car_y - sin(car_yaw);
+		    ptsx.push_back(prev_car_x);
+		    ptsx.push_back(car_x);
+		    ptsy.push_back(prev_car_y);
+		    ptsy.push_back(car_y);
+		}
+		else 
+		{
+		    /* Push the last 2 points from the previous path */
+		    ref_x = previous_path_x [prev_size-1];
+		    ref_y = previous_path_y [prev_size-1];
+
+		    double ref_x_prev = previous_path_x[prev_size-2]
+		    double ref_y_prev = previous_path_y[prev_size-2]
+
+		    ref_yaw  = atan2(ref_y-ref_y_prev,ref_x - ref_x_prev);
+
+		    ptsx.push_back(ref_x_prev);
+		    ptsx.push_back(ref_x);
+		    ptsy.push_back(ref_y_prev);
+		    ptsy.push_back(ref_y);
+		}
+
+
+		/* Create 3 more points for spline at s=30,60 & 90 */
+		vector <double> next_wp0 = getXY(car_s+30,(2 + 4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+		vector <double> next_wp0 = getXY(car_s+60,(2 + 4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+		vector <double> next_wp0 = getXY(car_s+90,(2 + 4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+
+		ptsx.push_back(next_wp0[0]);
+		ptsx.push_back(next_wp1[0]);
+		ptsx.push_back(next_wp2[0]);
+
+		ptsy.push_back(next_wp0[1]);
+		ptsy.push_back(next_wp1[1]);
+		ptsy.push_back(next_wp2[1]);
+
+		/* Convert coordinate to the origin of current car position */
+		for (int i=0;i<ptsx.size();i++)
+		{
+		    double shift_x = ptsx[i] - ref_x;
+		    double shift_y = ptsy[i] - ref_y;
+		    ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y *sin(0-ref_yaw));
+		    ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y *cos(0-ref_yaw));
+		}
+
+		tk:: spline s;
+
+		/* Set anchor points on spline */
+		s.set_points(ptsx,ptsy);
+
+		vector <double> next_x_vals;
+		vector <double> next_y_vals;
+		
+		/* Add remaining points from the previous path to the current          */
+		/* previous_path is the remaining points that the car didnt transverse */
+		for (int i=0;i <previous_path_x.size();i++)
+		{
+		    next_x_vals.push_back(previous_path_x[i]);
+		    next_y_vals.push_back(previous_path_y[i]);
+		}
+
+		double target_x = 30.0; // Split remaining points into equally spaced points for 30 meter
+		double target_y = s(target_x);
+		double target_dist = sqrt((target_x)*(target_x) + (target_y)*(target_y));
+
+		double x_add_on = 0;
+
+		/* Populate remaining points that were not covered by left over previous path */
+		for (int i=1;i<= 50 - previous_path.x_size();i++) {
+		    double N = (target_dist/(0.02 *ref_vel/2.24));  // Step is target_dist / (time * velocity)
+		    double x_point = x_add_on + (target_x)/N;
+		    double y_point = s(x_point);
+		    x_add_on = x_point;
+
+
+		    double x_ref = x_point;
+		    double y_ref = y_point;
+
+		    /* Convert coordinate back */
+		    x_point = (x_ref * cos(ref_yaw) - y_ref*sin(ref_yaw));
+		    y_point = (x_ref * sin(ref_yaw) + y_ref*cos(ref_yaw));
+
+		    x_point += ref_x;
+		    y_point += ref_y;
+		    next_x_vals.push_back(x_point);
+		    next_y_vals.push_back(y_point);
+
+		}
+
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
